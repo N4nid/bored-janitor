25bf8f20fc10e4d62d13ee51d9d3c86241f57967
MortySmith <63672127+Morty0Smith@users.noreply.github.com>
2025-02-28 22:39:04 +0100

    EnimyScriptWorking

--- BoredJanitor/Assets/Scripts/RaycastHelper.cs
+++ BoredJanitor/Assets/Scripts/RaycastHelper.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BurgerAttracks : MonoBehaviour
+public class RaycastHelper
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
