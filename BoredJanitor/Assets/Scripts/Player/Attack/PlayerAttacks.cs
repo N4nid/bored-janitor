9ddb98588848bc425642cedf836fb85df0e4a23c
cakePanCakes <165209840+cakePanCakes@users.noreply.github.com>
2025-02-28 23:10:17 +0100

    Added attacks

---
+++ BoredJanitor/Assets/Scripts/Player/Attack/PlayerAttacks.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerAttacks : MonoBehaviour
+{
+    public GameObject hitboxPreafab;
+    public HitboxInfo[,] HitboxInfos = new HitboxInfo[3,4];
+    //public ArrayList HitboxInfos = new ArrayList();
+    ArrayList hitboxen = new ArrayList();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HitboxInfos[0,0] = new HitboxInfo(new Vector3(4,1), new Vector2(2,-1)); //ligth
+        HitboxInfos[0,1] = new HitboxInfo(new Vector3(1.5f,4), new Vector2(1.25f,0)); //heavy
+    }
+
+    void CreateHitbox(int weaponTyp, int attackStyle){ //float offsetX, float offsetY, Vector3 size
+        HitboxInfo info = HitboxInfos[weaponTyp,attackStyle];
+        Vector3 myPos = new Vector3(transform.position.x + info.offset.x, transform.position.y + info.offset.y);
+        GameObject myHitbox = Instantiate(hitboxPreafab, myPos, Quaternion.identity);
+        BoxCollider2D box = myHitbox.GetComponent<BoxCollider2D>();
+        box.size = info.size;
+        myHitbox.transform.parent = this.gameObject.transform;
+        hitboxen.Add(myHitbox);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("starting attack");
+            CreateHitbox(0,0);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("starting attack");
+            CreateHitbox(0,1);
+        }
+    }
+
+
+}
