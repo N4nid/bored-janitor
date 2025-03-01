a9a94afe08a828958000cfb782fe71565e9d37e4
nanid <nanid@tuta.io>
2025-03-01 18:41:56 +0100

    Added Ability to pullup to ledges

--- BoredJanitor/Assets/Scripts/Player/PlayerMovement.cs
+++ BoredJanitor/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,14 +7,21 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 velocity;
     [SerializeField] GameObject player;
     [SerializeField] Rigidbody2D body;
-    float groundDistanceMargin = 0.1f;
-    public bool onGround = true;
-    [SerializeField] float jumpForce = 250f;
+    [SerializeField] float jumpForce;
+    [SerializeField] float pullUpForce;
     [SerializeField] float moveForce = 500f;
     [SerializeField] float maxSpeed = 20f;
-    bool isJumping = false;
     [SerializeField] float delay = 0.09f;
     [SerializeField] Animator playerAnime;
+    [SerializeField] float pullupRayLength = 1f;
+    [SerializeField] float pullupFactor = 0.3f;
+    [SerializeField] float test = 0.3f;
+    bool isJumping = false;
+    float direction;
+    float groundDistanceMargin = 0.1f;
+    public bool onGround = true;
+    public bool canDoPullUp = true;
+
 
 
     // Update is called once per frame
@@ -22,12 +29,20 @@ public class PlayerMovement : MonoBehaviour
     {
         playerAnime.SetFloat("xVel", Math.Abs(body.linearVelocityX));
         onGround = isOnGround();
-        playerAnime.SetBool("isHigh",!onGround);
+        canDoPullUp = canClimbLedge();
+
+        playerAnime.SetBool("isHigh", !onGround);
     }
 
     public void jump()
     {
-        if (onGround && !isJumping)
+        if (canDoPullUp && body.linearVelocityY <= 0)
+        {
+            //body.AddForceY(pullUpForce, ForceMode2D.Impulse);
+            body.AddForce(new Vector2(pullUpForce * direction * pullupFactor, pullUpForce), ForceMode2D.Impulse);
+            Debug.Log("LEDGE BOOOST ");
+        }
+        else if (onGround && !isJumping)
         {
             body.AddForceY(jumpForce, ForceMode2D.Impulse);
             Debug.Log("supposed to jump " + onGround);
@@ -44,32 +59,58 @@ public class PlayerMovement : MonoBehaviour
 
     public void move(bool left)
     {
-        float direction;
         if (left)
         {
             direction = -1f;
-            transform.localScale = new Vector2(-1,1);
+            transform.localScale = new Vector2(-1, 1);
         }
         else
         {
             direction = 1f;
-            transform.localScale = new Vector2(1,1);
+            transform.localScale = new Vector2(1, 1);
         }
         if (Math.Abs(body.linearVelocityX) < maxSpeed)
         {
-            Debug.Log("jsad" + body.linearVelocityX);
             body.AddForceX(moveForce * direction * Time.deltaTime);
         }
     }
+
+    bool canClimbLedge()
+    {
+        float height = 2.2f;
+        for (int i = 0; i < 3; i++)
+        {
+            Vector2 rayDirection = (direction == -1) ? Vector2.left : Vector2.right;
+            Vector3 origin = new Vector3(player.transform.localScale.x + (0.2f * direction), height + (i * 0.2f));
+            Debug.DrawRay(transform.position + origin, rayDirection * pullupRayLength, Color.green, 0.2f, true);
+            RaycastHit2D eyeLevel = Physics2D.Raycast(transform.position + origin, rayDirection, pullupRayLength);
+
+            if (eyeLevel.collider != null)
+            {
+
+                Debug.Log(eyeLevel.collider.gameObject.name + "  " + eyeLevel.collider.gameObject.tag + "   " + eyeLevel.distance);
+                if (eyeLevel.collider.gameObject.tag == "Platform")
+                {
+                    Debug.Log("CAN CLIMB LEDGE");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     bool isOnGround()
     {
         float width = 1f; // Player width 
-        float height = 2.4f;
         int numberCasts = 20;
         float widthIcrement = width / numberCasts;
-        for(int i = 0; i < numberCasts; i++) {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(-width / 2 + widthIcrement * i,- groundDistanceMargin), Vector2.down);
-            if (hit.collider != null && hit.distance <= 0){return true;}
+
+
+
+        for (int i = 0; i < numberCasts; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(-width / 2 + widthIcrement * i, -groundDistanceMargin), Vector2.down);
+            if (hit.collider != null && hit.distance <= 0) { return true; }
         }
         return false;
     }
