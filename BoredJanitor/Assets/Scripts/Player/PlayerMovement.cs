a12faf57e286df022f1ca0212b8713932c0d6829
nanid <nanid@tuta.io>
2025-03-01 13:23:58 +0100

    fixed jumping

--- BoredJanitor/Assets/Scripts/Player/PlayerMovement.cs
+++ BoredJanitor/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,9 +9,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody2D body;
     float groundDistanceMargin = 0.1f;
     public bool onGround = true;
-    [SerializeField] float jumpForce = 100f;
-    [SerializeField] float moveForce = 300f;
-    [SerializeField] float maxSpeed = 3f;  
+    [SerializeField] float jumpForce = 250f;
+    [SerializeField] float moveForce = 500f;
+    [SerializeField] float maxSpeed = 20f;
+    bool isJumping = false;
+    [SerializeField] float delay = 0.09f;
 
     void Start()
     {
@@ -20,20 +22,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!onGround)
-        {
-            onGround = isOnGround();
-        }
+        onGround = isOnGround();
     }
 
     public void jump()
     {
-        if (onGround)
+        if (onGround && !isJumping)
         {
-            body.AddForceY(jumpForce * Time.deltaTime, ForceMode2D.Impulse);
-            onGround = false;
+            body.AddForceY(jumpForce, ForceMode2D.Impulse);
             Debug.Log("supposed to jump " + onGround);
+            isJumping = true;
+            Invoke("resetJump", delay);
         }
+    }
+
+    void resetJump()
+    {
+        isJumping = false;
+        Debug.Log("reset Jump ");
     }
 
     public void move(bool left)
@@ -60,13 +66,13 @@ public class PlayerMovement : MonoBehaviour
         float height = 2.3f;
         RaycastHit2D downLookLeft = Physics2D.Raycast(transform.position - new Vector3(width / 2, 0f), Vector2.down);
         RaycastHit2D downLookRight = Physics2D.Raycast(transform.position + new Vector3(width / 2, 0f), Vector2.down);
-        Debug.DrawRay(transform.position - new Vector3(width / 2, 0f), Vector2.down, Color.yellow, 1);
-        Debug.DrawRay(transform.position + new Vector3(width / 2, 0f), Vector2.down, Color.yellow, 1);
+        //Debug.Log(downLookRight.collider.gameObject.name + "  " + downLookRight.distance);
+        //Debug.DrawRay(transform.position - new Vector3(width / 2, 0f), Vector2.down, Color.yellow, 1);
+        //Debug.DrawRay(transform.position + new Vector3(width / 2, 0f), Vector2.down, Color.yellow, 1);
         bool isOnGroundLeft = downLookLeft.collider != null && downLookLeft.distance < height / 2 + groundDistanceMargin;
         bool isOnGroundRight = downLookRight.collider != null && downLookRight.distance < height / 2 + groundDistanceMargin;
         if (isOnGroundLeft || isOnGroundRight)
         {
-            Debug.Log("YAY on ground");
             return true;
         }
         return false;
