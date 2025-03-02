164045b3257bad27285129b04787957d6869309b
nanid <nanid@tuta.io>
2025-03-02 15:14:14 +0100

    Added basic sound effects, soundManager and improved broom Heavyattack

--- BoredJanitor/Assets/Scripts/Player/PlayerManager.cs
+++ BoredJanitor/Assets/Scripts/Player/PlayerManager.cs
@@ -2,97 +2,80 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-  // Start is called once before the first execution of Update after the MonoBehaviour is created
-  [SerializeField] GameObject player;
-  [SerializeField] float motivation = 100; // acts as Health
-  [SerializeField] UiManager ui;
-  [SerializeField] AudioSource sound;
-  [SerializeField] AudioClip killEffect;
-  [SerializeField] AudioClip lightAttackSFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject player;
+    [SerializeField] float motivation = 100; // acts as Health
+    [SerializeField] UiManager ui;
+    [SerializeField] soundManager sound;
 
-  string motivationText = "Motivation: ";
-  int score = 0;
-  public int killCounter = 0; // changed in Eneymmanager
+    string motivationText = "Motivation: ";
+    int score = 0;
+    public int killCounter = 0; // changed in Eneymmanager
 
-  void Start()
-  {
-
-    ui.setMotivationText(motivationText + motivation);
-    Invoke("timer", 1);
-  }
-
-  void timer()
-  {
-    loseMotivation(10);
-    if (motivation > 0)
+    void Start()
     {
-      Invoke("timer", 1);
-    }
-  }
 
-  void playSound(string sfx)
-  {
-    switch (sfx)
-    {
-      case "killEffect":
-        sound.PlayOneShot(killEffect);
-        break;
-
-      case "lightAttack":
-        sound.PlayOneShot(lightAttackSFX);
-        break;
-
-      default:
-        break;
-    }
-  }
-
-
-  public void gainMotivation(float amount)
-  {
-    motivation += amount;
-    ui.setMotivationText(motivationText + motivation);
-    ui.updateMotivation(motivation, true);
-    playSound("killEffect");
-  }
-
-  public void loseMotivation(float amount)
-  {
-    motivation -= amount;
-    ui.setMotivationText(motivationText + motivation);
-    ui.updateMotivation(motivation, false);
-    if (motivation <= 0)
-    {
-      becomeBored();
+        ui.setMotivationText(motivationText + motivation);
+        Invoke("timer", 1);
     }
 
-  }
-
-  void becomeBored()
-  {
-    ui.setMotivationText("Im bored, yall suck");
-    //TODO make die
-  }
-
-  void finish()
-  {
-    //TODO make finish
-  }
-
-  void OnCollisionEnter2D(Collision2D collision)
-  {
-    if (collision.gameObject.tag.Equals("Finish"))
+    void timer()
     {
-      Debug.Log(collision.gameObject.name);
-      finish();
+        loseMotivation(10);
+        if (motivation > 0)
+        {
+            Invoke("timer", 1);
+        }
+    }
+
+
+
+    public void gainMotivation(float amount)
+    {
+        motivation += amount;
+        ui.setMotivationText(motivationText + motivation);
+        ui.updateMotivation(motivation, true);
+        sound.playSound("killEffect");
+    }
+
+    public void loseMotivation(float amount)
+    {
+        motivation -= amount;
+        ui.setMotivationText(motivationText + motivation);
+        ui.updateMotivation(motivation, false);
+        sound.playSound("damageEffect");
+        if (motivation <= 0)
+        {
+            becomeBored();
+        }
 
     }
-  }
+
+    void becomeBored()
+    {
+        ui.setMotivationText("Im bored, yall suck");
+        //TODO make die
+    }
+
+    void finish()
+    {
+        //TODO make finish
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Finish"))
+        {
+            Debug.Log(collision.gameObject.name);
+            finish();
+
+        }
+    }
 
 
-  // Update is called once per frame
-  void Update()
-  {
+    // Update is called once per frame
+    void Update()
+    {
 
-  }
+    }
 }
