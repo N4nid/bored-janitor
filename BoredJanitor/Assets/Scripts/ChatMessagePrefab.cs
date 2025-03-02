e6d9a6d7b8236bda9004b730d9af8e6b90326182
MortySmith <63672127+Morty0Smith@users.noreply.github.com>
2025-03-02 18:00:42 +0100

    Added twitch chat

--- BoredJanitor/Assets/Scripts/ChatMessagePrefab.cs
+++ BoredJanitor/Assets/Scripts/ChatMessagePrefab.cs
@@ -8,27 +8,39 @@ public class ChatMessagePrefab : MonoBehaviour
     [SerializeField] GameObject messageTextObj;
     TMP_Text usernameText;
     TMP_Text chatMessage;
-    [SerializeField] float charWidth = 19f;
+    [SerializeField] float charWidth = 25f;
+    [SerializeField] float maxWidth = 450;
     [SerializeField] float xOffset = 5f;
     RectTransform usernameTrans;
     RectTransform messageTrans;
+    public int lineCount;
     void Awake()
     {
         usernameText = usernameTextObj.GetComponent<TMP_Text>();
         chatMessage = messageTextObj.GetComponent<TMP_Text>();
         usernameTrans = usernameTextObj.GetComponent<RectTransform>();
-        messageTrans = usernameTextObj.GetComponent<RectTransform>();
+        messageTrans = messageTextObj.GetComponent<RectTransform>();
     }
 
-    public void setMessage(String username, String message) {
-        float usernameWidth = username.Length * charWidth;
+    public int setMessage(String username, String message) {
+        float usernameWidth = (username.Length + 1)* charWidth;
         float chatWidth = message.Length * charWidth;
         usernameTrans.anchoredPosition = new Vector2(usernameWidth/2 + xOffset,0f);
         usernameTrans.sizeDelta = new Vector2(usernameWidth,34f);
-        messageTrans.anchoredPosition = new Vector2(usernameWidth + 2*xOffset+chatWidth/2,0f);
+        messageTrans.anchoredPosition = new Vector2(usernameWidth + xOffset+chatWidth/2,0f);
         messageTrans.sizeDelta = new Vector2(chatWidth,34f);
         usernameText.text = username;
         chatMessage.text = message;
+        if (usernameWidth + chatWidth > maxWidth) {
+            Debug.Log("Hi");
+            messageTrans.sizeDelta = new Vector2(maxWidth,messageTrans.sizeDelta.y);
+            messageTrans.anchoredPosition = new Vector2(maxWidth/2 + xOffset,0f);
+            chatMessage.SetText("<space=" + usernameWidth + ">" + message);
+            lineCount = 2;
+            return 2;
+        }
+        lineCount = 1;
+        return 1;
     }
 
 }
